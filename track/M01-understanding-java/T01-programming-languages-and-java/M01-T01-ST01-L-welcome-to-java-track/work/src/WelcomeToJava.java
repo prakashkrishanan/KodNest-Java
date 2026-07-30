f117f735f2ba1dbf265e77be82fd96a97c27728b
prakash <prakashkrishnan>
2026-07-30 12:59:26 +0530

    added line

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -6,5 +6,6 @@ public class WelcomeToJava {
         System.out.println("I already know how to build programming logic.");
         System.out.println("Now I am learning how to express that logic using Java.");
         System.out.println("my idolo is msd");
+        System.out.println("now i know how to pust git");
     }
 }
