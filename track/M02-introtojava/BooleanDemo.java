5b45d18c65e588e993dca6156cab0440ea161e0c
prakash <prakashkrishnan>
2026-08-04 11:57:42 +0530

    BooleanDemo

---
+++ track/M02-introtojava/BooleanDemo.java
@@ -0,0 +1,12 @@
+public class BooleanDemo {
+    public static void main(String[] args) {
+        int solvedProblems = 24;
+        int requiredProblems = 20;
+
+        boolean targetReached = solvedProblems >= requiredProblems;
+
+        System.out.println("Solved problems: " + solvedProblems);
+        System.out.println("Required problems: " + requiredProblems);
+        System.out.println("Target reached: " + targetReached);
+    }
+}
