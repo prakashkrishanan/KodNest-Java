8db6deac81f2e0540dd1e7a7043be2af324eb84c
prakash <prakashkrishnan>
2026-08-04 12:29:22 +0530

    IfDemo

--- track/M02-introtojava/BooleanDemo.java
+++ track/M02-introtojava/BooleanDemo.java
@@ -8,5 +8,9 @@ public class BooleanDemo {
         System.out.println("Solved problems: " + solvedProblems);
         System.out.println("Required problems: " + requiredProblems);
         System.out.println("Target reached: " + targetReached);
+        int score = 68;
+
+        System.out.println(score >= 60);
+        System.out.println(score >= 70);
     }
 }
---
+++ track/M02-introtojava/IfDemo.java
@@ -0,0 +1,20 @@
+public class IfDemo{
+    public static void main(String[] args) {
+    int num = -9;
+    if (num>0)
+    {
+        System.out.println("positive");
+    }
+    else if(num<0){
+        System.out.println("negaive");
+    }
+    else
+    {
+        System.out.println("0");
+    }
+
+
+        
+    }
+    
+}
